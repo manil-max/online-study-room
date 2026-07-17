54f02acb67ac6a787dc61590206b2367db539c67
muhlis2 <muhlis2222muhlis@gmail.com>
2026-07-18 01:41:44 +0300

    chore: Windows plugin registrant after share_plus (WP-152)

--- app/windows/flutter/generated_plugin_registrant.cc
+++ app/windows/flutter/generated_plugin_registrant.cc
@@ -11,6 +11,7 @@
 #include <passkeys_windows/passkeys_windows_plugin.h>
 #include <screen_retriever_windows/screen_retriever_windows_plugin_c_api.h>
 #include <sentry_flutter/sentry_flutter_plugin.h>
+#include <share_plus/share_plus_windows_plugin_c_api.h>
 #include <url_launcher_windows/url_launcher_windows.h>
 #include <window_manager/window_manager_plugin.h>
 
@@ -25,6 +26,8 @@ void RegisterPlugins(flutter::PluginRegistry* registry) {
       registry->GetRegistrarForPlugin("ScreenRetrieverWindowsPluginCApi"));
   SentryFlutterPluginRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("SentryFlutterPlugin"));
+  SharePlusWindowsPluginCApiRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("SharePlusWindowsPluginCApi"));
   UrlLauncherWindowsRegisterWithRegistrar(
       registry->GetRegistrarForPlugin("UrlLauncherWindows"));
   WindowManagerPluginRegisterWithRegistrar(
--- app/windows/flutter/generated_plugins.cmake
+++ app/windows/flutter/generated_plugins.cmake
@@ -8,6 +8,7 @@ list(APPEND FLUTTER_PLUGIN_LIST
   passkeys_windows
   screen_retriever_windows
   sentry_flutter
+  share_plus
   url_launcher_windows
   window_manager
 )
